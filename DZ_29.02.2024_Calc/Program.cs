cfba136a052310de5122542b7290a77dadaac1ea
Андрей <dusha22075@gmail.com>
2024-02-23 12:57:20 +0300

    Добавьте файлы проекта.

---
+++ DZ_29.02.2024_Calc/Program.cs
@@ -0,0 +1,48 @@
+﻿class Calculator
+{
+    static void Main()
+    {
+        double num1, num2, result;
+        char op;
+
+        Console.WriteLine("Простой калькулятор");
+        Console.Write("Введите первое число: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите оператор (+, -, *, /): ");
+        op = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        switch (op)
+        {
+            case '+':
+                result = num1 + num2;
+                Console.WriteLine($"Результат: {num1} + {num2} = {result}");
+                break;
+            case '-':
+                result = num1 - num2;
+                Console.WriteLine($"Результат: {num1} - {num2} = {result}");
+                break;
+            case '*':
+                result = num1 * num2;
+                Console.WriteLine($"Результат: {num1} * {num2} = {result}");
+                break;
+            case '/':
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    Console.WriteLine($"Результат: {num1} / {num2} = {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: деление на ноль.");
+                }
+                break;
+            default:
+                Console.WriteLine("Некорректный оператор.");
+                break;
+        }
+    }
+}
